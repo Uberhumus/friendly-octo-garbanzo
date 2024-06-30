099c78d84bb13de74ba8db7cecacdb203388bef8
Yotam Glusman <uberhumus@gmail.com>
2024-06-30 13:56:14 +0300

    Added role and policies

--- tofu/eks-cluster.tf
+++ tofu/eks-cluster.tf
@@ -23,6 +23,27 @@ module "eks" {
     }
   }
 
+  iam_role_additional_policies = [
+    {
+      policy_name = "secrets_manager_read_policy"
+      policy      = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "secretsmanager:GetSecretValue",
+                "secretsmanager:DescribeSecret"
+            ],
+            "Resource": "arn:aws:secretsmanager:eu-west-1:975050008954:secret:SuperSecretNASAToken*"
+        }
+    ]
+}
+EOF
+    }
+  ]
+
   node_security_group_additional_rules = {
     open_app_port = {
       description              = "Allow inbound traffic on port 30001"
---
+++ tofu/iam.tf
@@ -0,0 +1,56 @@
+resource "aws_iam_user" "github_automation" {
+  name = "github-automation"
+}
+
+resource "aws_iam_access_key" "github_automation_access_key" {
+  user = aws_iam_user.github_automation.name
+}
+
+resource "aws_iam_policy" "ecr_policy" {
+  name        = "ECRFullAccessPolicy"
+  description = "Policy to allow full access to a specific ECR repository"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "ecr:*",
+        Resource = "arn:aws:ecr:eu-west-1:975050008954:repository/${var.cluster_name}"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "eks_policy" {
+  name        = "EKSFullAccessPolicy"
+  description = "Policy to allow full access to a specific EKS cluster"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = "eks:*",
+        Resource = "arn:aws:eks:eu-west-1:975050008954:cluster/${var.cluster_name}"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_user_policy_attachment" "ecr_policy_attachment" {
+  user       = aws_iam_user.github_automation.name
+  policy_arn = aws_iam_policy.ecr_policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "eks_policy_attachment" {
+  user       = aws_iam_user.github_automation.name
+  policy_arn = aws_iam_policy.eks_policy.arn
+}
+
+output "access_key_id" {
+  value = aws_iam_access_key.github_automation_access_key.id
+}
+
+output "secret_access_key" {
+  value     = aws_iam_access_key.github_automation_access_key.secret
+  sensitive = true
+}
