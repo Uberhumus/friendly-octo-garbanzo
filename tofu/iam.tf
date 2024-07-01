3114607d6ee3d48c64e1861a2fc12ad1b3c602ff
Yotam Glusman <uberhumus@gmail.com>
2024-07-01 11:53:47 +0300

    Corrected IAM Policies for Github

--- tofu/iam.tf
+++ tofu/iam.tf
@@ -6,62 +6,90 @@ resource "aws_iam_access_key" "github_automation_access_key" {
   user = aws_iam_user.github_automation.name
 }
 
-resource "aws_iam_policy" "ecr_policy" {
-  name        = "ECRFullAccessPolicy"
+resource "aws_iam_policy" "github_automation_ecr_policy" {
+  name        = "GithubECRAccessPolicy"
   description = "Policy to allow full access to a specific ECR repository"
   policy      = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow",
-        Action   = "ecr:*",
-      }
-    ]
+	"Version": "2012-10-17",
+	"Statement": [
+		{
+			"Sid": "VisualEditor0",
+			"Effect": "Allow",
+			"Action": [
+				"ecr:GetLifecyclePolicyPreview",
+				"ecr:GetDownloadUrlForLayer",
+				"ecr:ListTagsForResource",
+				"ecr:UploadLayerPart",
+				"ecr:ListImages",
+				"ecr:PutImage",
+				"ecr:UntagResource",
+				"ecr:CompleteLayerUpload",
+				"ecr:DescribeImages",
+				"ecr:TagResource",
+				"ecr:DescribeRepositories",
+				"ecr:InitiateLayerUpload",
+				"ecr:ReplicateImage",
+				"ecr:GetRepositoryPolicy",
+				"ecr:GetLifecyclePolicy"
+			],
+			"Resource": "arn:aws:ecr:eu-west-1:975050008954:repository/home-assignment-yotam"
+		},
+		{
+			"Sid": "VisualEditor1",
+			"Effect": "Allow",
+			"Action": [
+				"ecr:GetRegistryPolicy",
+				"ecr:DescribeRegistry",
+				"ecr:GetAuthorizationToken"
+			],
+			"Resource": "*"
+		}
+	]
   })
 }
 
-resource "aws_iam_policy" "eks_policy" {
-  name        = "EKSFullAccessPolicy"
+resource "aws_iam_policy" "github_automation_eks_policy" {
+  name        = "GithubEKSAccessPolicy"
   description = "Policy to allow full access to a specific EKS cluster"
   policy      = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
+    "Version" = "2012-10-17",
+    "Statement" = [
       {
-        Effect   = "Allow",
-        Action   = "eks:*",
+        "Effect"   = "Allow",
+        "Action"   = "eks:*",
+        "Resource" = "*"
       }
     ]
   })
 }
 
 resource "aws_iam_policy" "secrets_manager_read_policy" {
-  name        = "secrets_manager_read_policy"
+  name        = "eks_secrets_manager_read_policy"
   description = "Policy to allow read-only access to specific secrets"
-  policy      = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "secretsmanager:GetSecretValue",
-                "secretsmanager:DescribeSecret"
-            ],
-            "Resource": "arn:aws:secretsmanager:eu-west-1:975050008954:secret:SuperSecretNASAToken*"
-        }
+  policy      = jsonencode({
+    "Version"= "2012-10-17",
+    "Statement"= [
+      {
+        "Effect"= "Allow",
+        "Action"= [
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:DescribeSecret"
+        ],
+        "Resource"= "arn:aws:secretsmanager:eu-west-1:975050008954:secret:SuperSecretNASAToken*"
+      }
     ]
-}
-EOF
-}
-
-resource "aws_iam_user_policy_attachment" "ecr_policy_attachment" {
-  user       = aws_iam_user.github_automation.name
-  policy_arn = aws_iam_policy.ecr_policy.arn
+  
+  })
 }
 
-resource "aws_iam_user_policy_attachment" "eks_policy_attachment" {
+resource "aws_iam_user_policy_attachment" "github_automation_ecr_policy_attachment" {
   user       = aws_iam_user.github_automation.name
-  policy_arn = aws_iam_policy.eks_policy.arn
+  policy_arn = aws_iam_policy.github_automation_ecr_policy.arn
+}
+
+resource "aws_iam_user_policy_attachment" "github_automation_eks_policy_attachment" {
+  user       = aws_iam_user.github_automation.name
+  policy_arn = aws_iam_policy.github_automation_eks_policy.arn
 }
 
 output "access_key_id" {
